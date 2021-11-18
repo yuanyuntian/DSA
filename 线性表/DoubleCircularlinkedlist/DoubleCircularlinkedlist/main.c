5c8d71f33a7be85a90f91120b3d579a711ca5a8a
yuanyuntian <yuanf1990@163.com>
2021-11-18 18:27:23 +0800

    删除

--- 线性表/DoubleCircularlinkedlist/DoubleCircularlinkedlist/main.c
+++ 线性表/DoubleCircularlinkedlist/DoubleCircularlinkedlist/main.c
@@ -64,8 +64,112 @@ void insertAfter(struct Node * prev_node, int new_data) {
     }
 }
 
+//give a reference to the head of a DLL and an int , appends a new node at the end
+void append(struct Node ** head_ref, int new_data) {
+    //allocate node
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    
+    struct Node * last = *head_ref;
+    
+    new_node->data = new_data;
+    
+    //this new node is going to be the last node, so make next of it as NULL
+    new_node->next = NULL;
+    
+    //if the linked list is empty, then make the new node as head
+    if (*head_ref == NULL) {
+        new_node->prev = NULL;
+        *head_ref = new_node;
+        return;
+    }
+    
+    // traverse till the last node
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    
+    //change the next of last node
+    last->next = new_node;
+    
+    //make last node as previous of new node
+    new_node->prev = last;
+    return;
+}
+
+// This function prints contents of linked list starting
+// from the given node
+void printList(struct Node* node)
+{
+    struct Node* last;
+    printf("\nTraversal in forward direction \n");
+    while (node != NULL) {
+        printf(" %d ", node->data);
+        last = node;
+        node = node->next;
+    }
+ 
+    printf("\nTraversal in reverse direction \n");
+    while (last != NULL) {
+        printf(" %d ", last->data);
+        last = last->prev;
+    }
+}
+
+//节点的删除一般在三个位置：头结点，中间，尾部。针对这些位置，我们只需要按照两种情况来处理
+//1.如果删除的节点是头部，只需要把头结点的下一个节点设置成头结点即可
+//2.如果不是头结点，只需要关联它的前节点和后节点即可
+
+void deleteNode(struct Node ** head_ref, struct Node * del) {
+    if (*head_ref == NULL || del == NULL) {
+        return;
+    }
+    
+    //if node to be deleted is head node
+    if (*head_ref == del) {
+        *head_ref = del->next;
+    }
+    
+    //change next only if node to be deleted is NOT the last node
+    if (del->next != NULL) {
+        del->next->prev = del->prev;
+    }
+    
+    //change prev only if node to be deleted is NOT the first node
+    if (del->prev != NULL) {
+        del->prev->next = del ->next;
+    }
+    
+    //finally , free the memory
+    free(del);
+    return;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    /* Start with the empty list */
+      struct Node* head = NULL;
+   
+      // Insert 6.  So linked list becomes 6->NULL
+      append(&head, 6);
+   
+      // Insert 7 at the beginning. So linked list becomes
+      // 7->6->NULL
+      push(&head, 7);
+   
+      // Insert 1 at the beginning. So linked list becomes
+      // 1->7->6->NULL
+      push(&head, 1);
+   
+      // Insert 4 at the end. So linked list becomes
+      // 1->7->6->4->NULL
+      append(&head, 4);
+   
+      // Insert 8, after 7. So linked list becomes
+      // 1->7->8->6->4->NULL
+      insertAfter(head->next, 8);
+   
+      printf("Created DLL is: ");
+      printList(head);
+    
     return 0;
 }
